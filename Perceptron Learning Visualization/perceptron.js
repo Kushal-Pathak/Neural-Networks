2419268a62b9f033a97c025705d3ee2548c4b781
Kushal Pathak <kushalpathak80@gmail.com>
2024-05-19 20:48:38 +0545

    improved code and refactored

--- Perceptron Learning Visualization/perceptron.js
+++ Perceptron Learning Visualization/perceptron.js
@@ -2,8 +2,8 @@ class Perceptron {
   constructor() {
     this.lr = 0.1; //learning rate
     this.bias = 1; //bias value
-    this.w1 = random(-1, 1); //first weight
-    this.w2 = random(-1, 1); //second weight
+    this.w1 = random(-1,1); //first weight
+    this.w2 = random(-1,1); //second weight
   }
 
   //weighted sum of inputs
@@ -18,31 +18,60 @@ class Perceptron {
     return 0;
   }
 
-  //learn on single data
-  learn(x1, x2, target) {
-    let result = this.predict(x1, x2);
-    let error = target - result;
-    this.w1 += this.lr * error * x1;
-    this.w2 += this.lr * error * x2;
-    this.bias += this.lr * error;
-    stroke(0);
-    strokeWeight(2);
-    if (error == 0) fill(0, 255, 0);
-    else fill(255, 0, 0);
-    circle(x1, x2, 15);
-  }
-
-  //train on lots of data
-  train(data) {
-    for (let i = 0; i < data.length; i++) {
-      this.learn(data[i].x, data[i].y, data[i].z);
-    }
-  }
-
   //predict the test set
   predict(x1, x2) {
     let prediction = this.linear_combiner(x1, x2);
     prediction = this.activation_function(prediction);
     return prediction;
   }
+
+  //learn on single data
+  learn(x1, x2, target) {
+    let result = this.predict(x1, x2);
+    let error = target - result;
+    if (error == 0) return true; //do not learn if error is zero
+    this.w1 += this.lr * error * x1; //update w1
+    this.w2 += this.lr * error * x2; //update w2
+    this.bias += this.lr * error; //update bias
+    return false; //return false if error is non zero
+  }
+
+  //learn on lots of data
+  train(data) {
+    let trained = true;
+    for (let i = 0; i < data.length; i++) {
+      let result = this.learn(data[i].x, data[i].y, data[i].z);
+      trained = trained && result;
+    }
+    return trained; //returns true if training is completed
+  }
+}
+
+//generate coordinate plane points
+function generateTrainingData(x_max, y_max) {
+  for (let i = 0; i < totaldata; i++) {
+    let random_x = random(0, x_max);
+    let random_y = random(0, y_max);
+    let group = random_x > random_y ? 1 : -1;
+    let d = createVector(random_x, random_y, group);
+    trainingData.push(d);
+  }
+}
+
+function showLearnedBoundary(p) {
+  let w1 = p.w1;
+  let w2 = p.w2;
+  let b = p.bias;
+  let x1 = 0;
+  let x2 = width;
+  let y1 = -(x1 * w1 + b) / w2;
+  let y2 = -(x2 * w1 + b) / w2;
+  stroke(0)
+  line(0, 0, width, height); //original boundary
+  stroke(0,0,0,75)
+  strokeWeight(8)
+  line(x1, y1, x2, y2); //transparent thick black line
+  strokeWeight(2)
+  stroke(0)
+  line(x1, y1, x2, y2); //opaque thin black line
 }
--- Perceptron Learning Visualization/sketch.js
+++ Perceptron Learning Visualization/sketch.js
@@ -1,72 +1,67 @@
-let data = [];
-let p;
-let epoch = 0;
-let button;
+let perceptron; //our perceptron
+let trainingData = []; //training data's array
+let totaldata = 200; //total number of training data to use
+let iteration = 0; //to track the number of data points trained in an epoch
+let epoch = 0; //to track total number of epochs
+let div; //to show prediction text
+let alpha; //slider to adjust learning rate
+
 function setup() {
-  createCanvas(745, 665);
-  background(220);
-  button = createButton("Next Epoch");
-  button.position(width / 2 - 10, height - 25);
-  strokeWeight(1.5);
-  line(50, 50, width - 50, height - 50);
-  p = new Perceptron();
-  let i = 0;
-  while (i < 10) {
-    let x = random(50, width - 50);
-    let y = random(50, height - 50);
-    if (abs(x - y) <= 10) {
-      i++;
-      let z = x > y ? 1 : -1;
-      data.push(createVector(x, y, z));
-    }
-  }
-  for (let i = 0; i < 100; i++) {
-    let x = random(50, width - 50);
-    let y = random(50, height - 50);
-    let z = x > y ? 1 : -1;
-    data.push(createVector(x, y, z));
-  }
-  strokeWeight(2);
-  for (let point of data) {
-    fill(255, 0, 0);
-    circle(point.x, point.y, 15);
-  }
-  textSize(20);
-  strokeWeight(3);
-  fill(0);
-  text(`Epoch ${epoch}`, width - 100, 25);
+  createCanvas(720, 720);
+  generateTrainingData(width, height); //generating training data
+  div = createDiv("Prediction: ");
+  div.position(width + 10, 5);
+  div.style("font-size", "20px");
+  div.style("font-family", "sans-serif");
+  div.style("font-weight", "bold");
+  alpha = createSlider(0.001, 100, 50, 0);
+  alpha.position(width + 10, 35);
+  alpha.size(145);
+  perceptron = new Perceptron();
 }
 
 function draw() {
-  // background(220);
-  button.mousePressed(startEpoch);
-}
-
-function startEpoch() {
-  epoch++;
-  stroke(220);
-  strokeWeight(0);
-  fill(220);
-  rect(0, 0, width, 35);
-
-  strokeWeight(1);
-  fill(0);
-  text(`Epoch ${epoch}`, width - 100, 25);
-
-  p.train(data);
-  let a = 100;
-  let b = 101;
-  let result = p.predict(a, b);
-  //console.clear()
-  console.log(`Prediction for ${a},${b}: `, result);
-  console.log(`Actual class for ${a},${b}: `, -1);
-
-  strokeWeight(1);
-  fill(0);
-  stroke(220);
-  text(`Predicting class for ${a},${b}: ${result}`, 25, 25);
-  fill(0, 0, 255);
-  strokeWeight(4);
+  frameRate(100);
+  background(220);
+  perceptron.lr = alpha.value();
+  let data = trainingData[iteration];
+  perceptron.learn(data.x, data.y, data.z);
+  iteration++;
+  for (data of trainingData) {
+    let result = perceptron.predict(data.x, data.y);
+    if (result == data.z) {
+      fill(0, 255, 0);
+    } else {
+      fill(255, 0, 0);
+    }
+    strokeWeight(2);
+    if (data.z == 1) {
+      push();
+      translate(data.x, data.y);
+      rectMode(CENTER);
+      rect(0, 0, 10, 10);
+      pop();
+    } else {
+      circle(data.x, data.y, 12);
+    }
+  }
+  if (iteration == totaldata) iteration = 0;
+  push();
   stroke(0);
-  circle(a, b, 20);
+  showLearnedBoundary(perceptron);
+  pop();
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    var prediction = perceptron.predict(mouseX, mouseY);
+    push();
+    noFill();
+    stroke(0, 0, 255);
+    circle(mouseX, mouseY, 20);
+    strokeWeight(10);
+    stroke(0);
+    point(mouseX, mouseY);
+    pop();
+  } else {
+    prediction = null;
+  }
+  div.html(`Prediction: ${prediction}`);
 }
